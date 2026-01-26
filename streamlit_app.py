27a007074d232a9d2b97afdb64fb99f2b3603d4f
leytisha-prog <lejack@syr.edu>
2026-01-26 15:56:21 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from openai import OpenAI
 
-create_page = st.Page('create.py', title='Create entry', icon=':material/add_circle')
+create_page = st.Page('Lab2_app.py', title='Create entry', icon=':material/add_circle')
 delete_page = st.Page('delete.py', title='Delete entry', icon=':material/delete')
 
 pg = st.navigation( [create_page, delete_page],)
