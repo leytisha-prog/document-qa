749165a8b64eb0c64ef4da74a4c6589950f8e26d
leytisha-prog <lejack@syr.edu>
2026-01-26 15:54:15 +0000

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 from openai import OpenAI
 
+create_page = st.Page('create.py', title='Create entry', icon=':material/add_circle')
+delete_page = st.Page('delete.py', title='Delete entry', icon=':material/delete')
+
+pg = st.navigation( [create_page, delete_page],)
+st.set_page_config(page_title="My Streamlit App", page_icon=':material/edit:')
+pg.run()
+
+
+
+
+
 # Default parameters
 st.set_page_config(page_title="OpenAI Streamlit App", page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
 
