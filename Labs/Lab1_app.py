77762f53bea62d3fc1055d84c0b5caaaa9df1960
leytisha-prog <lejack@syr.edu>
2026-01-26 16:22:50 +0000

    update

--- Labs/Lab1_app.py
+++ Labs/Lab1_app.py
@@ -89,9 +89,3 @@ st.set_page_config(
             'About': "This is a simple Streamlit app using OpenAI's API."
         }
     )
-create_page = st.Page('create.py', title='Create entry', icon=':material/add_circle')
-delete_page = st.Page('delete.py', title='Delete entry', icon=':material/delete')
-
-pg = st.navigation( [create_page, delete_page],)
-st.set_page_config(page_title="My Streamlit App", page_icon=':material/edit:')
-pg.run() 
