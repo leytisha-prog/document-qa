fd14216fb88ab28a9f868ad3e38607810c65a021
leytisha-prog <lejack@syr.edu>
2026-01-26 18:25:55 +0000

    update

--- Labs/Lab2_app.py
+++ Labs/Lab2_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("📄 Leytisha's Lab 1 App")
+st.title("📄 Leytisha's Lab 2 App")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
