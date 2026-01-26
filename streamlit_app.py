ac08921ec0c3ff6ed9daafd66be47fc8c013418f
leytisha-prog <lejack@syr.edu>
2026-01-26 17:17:53 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,9 @@ st.set_page_config(
 
 
 # Create pages for navigation
-Lab1_page = st.Page("Labs/Lab1_app.py", title="Lab 1", icon="desktop_computer")
-Lab2_page = st.Page("Labs/Lab2_app.py", title="Lab 2", icon="robot_face")
-delete_page = st.Page('delete.py', title='Delete entry', icon=':material/delete')
+Lab1_page = st.Page("Labs/Lab1_app.py", title="Lab 1", icon=":page_facing_up:")
+Lab2_page = st.Page("Labs/Lab2_app.py", title="Lab 2", icon=":robot_face:")
+delete_page = st.Page('delete.py', title='Delete entry', icon=':material/delete:')
 
 pg = st.navigation( [Lab1_page, Lab2_page, delete_page],)
 st.set_page_config(page_title="My Streamlit App", page_icon=':material/edit:')
