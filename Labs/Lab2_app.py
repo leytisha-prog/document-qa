aa251f1604b644a90c16a9f692045e5e348ee281
leytisha-prog <lejack@syr.edu>
2026-01-28 17:43:52 +0000

    update

--- Labs/Lab2_app.py
+++ Labs/Lab2_app.py
@@ -11,7 +11,7 @@ def read_pdf(uploaded_file):
     return text
 
 # Show title and description.
-st.title("Leytisha's HW 1 App")
+st.title("Leytisha's Lab 2 - Document Summarizer App")
 st.write(
     "Upload a PDF or a TXT document below and ask a question about it – GPT will answer! "
     #"To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
