45390875b6abe88ecce19df1d34f02f59f24f4c0
leytisha-prog <lejack@syr.edu>
2026-01-28 16:47:09 +0000

    update

--- Labs/Lab2_app.py
+++ Labs/Lab2_app.py
@@ -14,7 +14,7 @@ def read_pdf(uploaded_file):
 st.title("Leytisha's HW 1 App")
 st.write(
     "Upload a PDF or a TXT document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+    #"To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
@@ -64,7 +64,7 @@ if "OPEN_AI_KEY" in st.secrets:
                     messages=messages,
                     stream=True,
                 )
-                
+
             # Stream the response to the app using `st.write_stream`.
                 st.write_stream(stream)
  
