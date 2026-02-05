9d57e755f7e6b33da80f48f7d32d5232e7cfcd41
leytisha-prog <lejack@syr.edu>
2026-02-05 16:51:58 +0000

    update

--- Labs/Lab3_app.py
+++ Labs/Lab3_app.py
@@ -14,13 +14,6 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 
-with st.chat_message("user"):
-    st.markdown("Hello! How can I assist you today?")
-    st.write("Hello icon:wave: how can I assist you today?")
-    st.bar_chart(np.random.random(30, 3))
 
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"user has sent the following prompt: {prompt}")
 
 
