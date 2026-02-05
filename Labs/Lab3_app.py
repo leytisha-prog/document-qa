15fefcad1cffce5d96c36354b43c815feb85cbfa
leytisha-prog <lejack@syr.edu>
2026-02-05 18:00:22 +0000

    update

--- Labs/Lab3_app.py
+++ Labs/Lab3_app.py
@@ -33,13 +33,20 @@ if prompt := st.chat_input("Hello! I'm Chatty G, your AI assistant. How can I he
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history 
     st.session_state.messages.append({"role": "user", "content": prompt})
-        
-    response = f"Echo: {prompt}"
-    # Display assistant response in chat message container 
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+
+# Display assistant response in chat message container
+with st.chat_message("assistant"):
+     stream = client.chat.completions.create(
+         model=st.session_state["openai_model"],
+         messages=[
+            {"role": m["role"], "content": m["content"]}
+            for m in st.session_state.messages
+         ],
+         stream=True,
+     )
+     response = st.write_stream(stream)
+st.session_state.messages.append({"role": "assistant", "content": response})        
+   
 
 
 
