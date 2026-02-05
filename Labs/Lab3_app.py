750488699e32cecc21090a5f39e5d997ad4e925e
leytisha-prog <lejack@syr.edu>
2026-02-05 16:40:12 +0000

    update

--- Labs/Lab3_app.py
+++ Labs/Lab3_app.py
@@ -1,7 +1,15 @@
 import streamlit as st
 from openai import OpenAI 
+import numpy as np
+
 
 with st.chat_message("user"):
     st.markdown("Hello! How can I assist you today?")
-    st.write("Hello icon=:wave: how can I assist you today?")
-    
+    st.write("Hello icon:wave: how can I assist you today?")
+    st.bar_chart(np.random.random(30, 3))
+
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"user has sent the following prompt: {prompt}")
+
+
