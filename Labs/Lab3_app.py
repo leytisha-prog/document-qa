d5e4635c9a0d52b4ac95ef578d75479d1b9510d6
leytisha-prog <lejack@syr.edu>
2026-02-05 18:38:19 +0000

    update

--- Labs/Lab3_app.py
+++ Labs/Lab3_app.py
@@ -23,8 +23,9 @@ if "messages" not in st.session_state:
 
 # Display chat messages from history on app rerun 
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    if message["role"] != "system":
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
 # React to user input
 if prompt := st.chat_input("What would you like to ask Chatty G?"):
