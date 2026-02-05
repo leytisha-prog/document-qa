93c186678a96c8bbb3b64d4fa958d6b5d0eca8cd
leytisha-prog <lejack@syr.edu>
2026-02-05 16:51:33 +0000

    update

--- Labs/Lab3_app.py
+++ Labs/Lab3_app.py
@@ -2,6 +2,17 @@ import streamlit as st
 from openai import OpenAI 
 import numpy as np
 
+st.title ("Chatty G - Lab 3: Streamlit Chat Interface")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from histoty on app rerun 
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
 
 with st.chat_message("user"):
     st.markdown("Hello! How can I assist you today?")
