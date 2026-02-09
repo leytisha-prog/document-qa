80df2bec0c48f6445580f980fdf0bb02e1d548e9
leytisha-prog <lejack@syr.edu>
2026-02-09 19:03:47 +0000

    update

--- Labs/Lab4_app.py
+++ Labs/Lab4_app.py
@@ -4,3 +4,11 @@ import sys
 import chromadb
 from pathlib import Path 
 from PyPDF2 import PdfReader
+
+# A fix
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+# Create ChromaDB client 
+chroma_client = chromadb.PersistentClient(path='./ChromaDB_for_Lab')
+collection = chroma_client.get_or_create_collection('Lab4Collection')
