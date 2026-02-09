05f5e4a47471c248b24ac40e4b945c0abf437a94
leytisha-prog <lejack@syr.edu>
2026-02-09 18:51:26 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,9 @@ st.set_page_config(
 Lab1_page = st.Page("Labs/Lab1_app.py", title="Lab 1", icon="📄")
 Lab2_page = st.Page("Labs/Lab2_app.py", title="Lab 2", icon="🧪")
 Lab3_page = st.Page("Labs/Lab3_app.py", title="Lab 3", icon="🔬")
+Lab4_page = st.Page("Labs/Lab4_app.py", title="Lab 4", icon="ruler")
 
 
-pg = st.navigation( [Lab1_page, Lab2_page, Lab3_page])
+pg = st.navigation( [Lab1_page, Lab2_page, Lab3_page, Lab4_page])
 st.set_page_config(page_title="My Streamlit App", page_icon=':material/edit:')
 pg.run() 
