2d1bf1b685079b83b98b67d9f6328fe989d3231e
leytisha-prog <lejack@syr.edu>
2026-02-10 15:11:27 +0000

    update

--- Labs/Lab4_app.py
+++ Labs/Lab4_app.py
@@ -1,10 +1,12 @@
+
+import sys
+
 # A fix
 __import__('pysqlite3')
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 from openai import OpenAI
-import sys
 import chromadb
 from pathlib import Path 
 from PyPDF2 import PdfReader
