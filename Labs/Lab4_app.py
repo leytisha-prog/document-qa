876a714b8b092913e3aa18707e64b5d22b511237
leytisha-prog <lejack@syr.edu>
2026-02-11 00:01:33 +0000

    update

--- Labs/Lab4_app.py
+++ Labs/Lab4_app.py
@@ -58,7 +58,7 @@ def extract_text_from_pdf(pdf_path: str) -> str:
     text = "\n".join(pages_text).strip()
     return text
 
-folder_path = "./Labs/"
+folder_path = "./Labs/pdf_files"
 #### ----- POPULATE COLLECTION WITH PDFs ------ ####
 # This function uses extract_text_from_pdf
 # and add_to_collection to put syllabi in ChromDB collection 
@@ -97,6 +97,44 @@ def load_pdfs_to_collection(folder_path, str, collection) -> int:
 
 # 6 UI/QUERY LOGIC
 st.header("Ask a question about the syllabi")
+
+### --------- MAIN APP --------- ###
+st.title('Lab 4: Chatbot Using RAG')
+
+### --------- QUERYYING A COLLECTION - ONLY USED FOR TESTING --------- ###
+topic = st.sidebar.text-input('Topic', placeholder='Type your topic (e.g., GenAI)...')
+
+if topic:
+    client = st.session_state.openai_client
+    response = client.embeddings.create(
+        input=topic,
+        model='text-embedding-3-small')
+    
+    # Get the embedding
+    query_embedding = response.data[0].embedding
+
+    # Get the text related to this question (this prompt)
+    results = collection.query(
+        query_embeddings=[query_embedding],
+        n_results=3 # The number of closest documents to return
+    )
+    
+    # Display the results
+    st.subheader(f'Results for: {topic}')
+
+    for i in range(len(results['documents'][0])):
+        doc = results['documents'][0][i]
+        doc_id = results['ids'][0][i]
+
+        st.write(f'**{i+1}. {doc_id}**')
+
+else:
+    st.info('Enter a topic in the sidebar to search the collection')
+
+    
+    
+    
+
 
     
 
