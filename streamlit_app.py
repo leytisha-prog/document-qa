2233b0002c10879ab58fb8de0259d0dc1e5a542a
leytisha-prog <lejack@syr.edu>
2026-02-12 06:03:13 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,13 @@ Lab2_page = st.Page("Labs/Lab2_app.py", title="Lab 2", icon="🧪")
 Lab3_page = st.Page("Labs/Lab3_app.py", title="Lab 3", icon="🔬")
 Lab4_page = st.Page("Labs/Lab4_app.py", title="Lab 4", icon="📄") 
                 
+pg = st.navigation([
+    st.Page("Labs/Lab1_app.py", title="Lab 1"),
+    st.Page("Labs/Lab2_app.py", title="Lab 2"),
+    st.Page("Labs/Lab3_app.py", title="Lab 3"),
+    st.Page("Labs/Lab4_app.py", title="Lab 4", default=True), 
+])
+pg.run()
 
 
 pg = st.navigation( [Lab1_page, Lab2_page, Lab3_page, Lab4_page])
