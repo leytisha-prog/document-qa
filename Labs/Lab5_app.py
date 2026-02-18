575de35fa9cf209f97e1a2078222a980dc82a87d
leytisha-prog <lejack@syr.edu>
2026-02-18 16:06:39 +0000

    update

--- Labs/Lab5_app.py
+++ Labs/Lab5_app.py
@@ -5,6 +5,7 @@ location = "Syracuse, NY, USA"
 
 # FUNCTION to get current weather data
 api_key = st.secrets["weather_api_key"]
+Base_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 def get_current_weather(location, api_key, units="imperial"):
     url=(
