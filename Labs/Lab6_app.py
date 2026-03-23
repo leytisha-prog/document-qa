841dc6cd5f940d3365b45a63570dfcdfb454eac9
leytisha-prog <lejack@syr.edu>
2026-03-23 17:47:49 +0000

    update

---
+++ Labs/Lab6_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from openai import OpenAI 
+import pydantic
+
+# Set OpenAI API key from Streamlit secrets
+client = OpenAI(api_key=st.secrets["OPEN_AI_KEY"])
+
+# Show title and description.
+st.title("Lab 6 App - issued by Dre|Eddie|Jake")
+user_question = st.text_input(
+    "Type your question here..."
+)
+
+if user_question:
+    response = client.responses.create(
+        model="gpt-5",
+        instructions="You are a helpful assistant.",
+        input=user_question
+    )
+    response = client.response.create(
+        model="gpt-5", input="What is an agent?")
+    print(response.output_text)
+    
+    
--- streamlit_app.py
+++ streamlit_app.py
@@ -17,12 +17,12 @@ Lab1_page = st.Page("Labs/Lab1_app.py", title="Lab 1", icon="📄")
 Lab2_page = st.Page("Labs/Lab2_app.py", title="Lab 2", icon="🧪")
 Lab3_page = st.Page("Labs/Lab3_app.py", title="Lab 3", icon="🔬")
 Lab4_page = st.Page("Labs/Lab4_app.py", title="Lab 4", icon="🤖")
-
+Lab5_page = st.Page("Labs/Lab5_app.py", title="Lab 5", icon="☀️")
 
 # ✅ Make Lab 5 the default
-Lab5_page = st.Page(
-    "Labs/Lab5_app.py",
-    title="Lab 5",
+Lab6_page = st.Page(
+    "Labs/Lab6_app.py",
+    title="Lab 6",
     icon="☀️",
     default=True
 )
