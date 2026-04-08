71476545ec628ba2dced0eb34aa51cb1c1941f6d
leytisha-prog <lejack@syr.edu>
2026-04-08 13:50:13 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,12 +18,13 @@ Lab2_page = st.Page("Labs/Lab2_app.py", title="Lab 2", icon="🧪")
 Lab3_page = st.Page("Labs/Lab3_app.py", title="Lab 3", icon="🔬")
 Lab4_page = st.Page("Labs/Lab4_app.py", title="Lab 4", icon="🤖")
 Lab5_page = st.Page("Labs/Lab5_app.py", title="Lab 5", icon="☀️")
+Lab6_page = st.Page("Labs/Lab6_app.py", title="Lab 6", icon="🤖")
 
-# ✅ Make Lab 6 the default
-Lab6_page = st.Page(
-    "Labs/Lab6_app.py",
-    title="Lab 6",
-    icon="🤖",
+# ✅ Make Lab 8 the default
+Lab8_page = st.Page(
+    "Labs/Lab8_app.py",
+    title="Lab 8",
+    icon="🔗",
     default=True
 )
 
