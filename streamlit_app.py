9da96381ab370c911c3242bc1c87cd796df7dca6
leytisha-prog <lejack@syr.edu>
2026-04-09 03:32:06 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,5 +31,5 @@ Lab8_page = st.Page(
 # ---------------------------------------------------
 # Navigation
 # ---------------------------------------------------
-pg = st.navigation([Lab1_page, Lab2_page, Lab3_page, Lab4_page, Lab5_page, Lab6_page])
+pg = st.navigation([Lab1_page, Lab2_page, Lab3_page, Lab4_page, Lab5_page, Lab6_page, Lab8_page])
 pg.run()    
